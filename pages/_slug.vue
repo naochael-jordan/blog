00b42b4060b28741ce2cf5894b45773d918c8209
naochael-jordan <naochael.jordan@gmail.com>
2018-09-29 20:56:22 +0900

    feat: _slug vue

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,16 +1,56 @@
 <template>
-  <div>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <p class="date">{{ format(date, 'YYYY-MM-DD') }}</p>
     <div v-html="content"></div>
   </div>
 </template>
 
 <script>
+import { format } from "date-fns";
+
 export default {
   async asyncData({ params }) {
     const content = await import(`~/static/${params.slug}.md`);
     return {
       content
     };
+  },
+
+  computed: {
+    post: function() {
+      return this.$store.state.posts.find(
+        post => post.fileName === this.$route.path.replace("/", "")
+      );
+    },
+
+    title: function() {
+      return this.post.attributes.title;
+    },
+
+    date: function() {
+      return this.post.attributes.date;
+    }
+  },
+
+  methods: {
+    format
   }
 };
 </script>
+
+<style lang="scss" scoped>
+@import "~/assets/css/variables.scss";
+
+.container {
+  width: $width;
+  margin: 50px auto;
+
+  h1 {
+  }
+
+  .date {
+    text-align: right;
+  }
+}
+</style>
