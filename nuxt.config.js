5d150ff6feb132a4419e7af52ff6a64fc12a0f2c
naochael-jordan <naochael.jordan@gmail.com>
2018-10-02 18:41:44 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const fm = require('front-matter');
+const fm = require("front-matter");
 const parseArgs = require("minimist");
 const argv = parseArgs(process.argv.slice(2), {
   alias: {
@@ -131,49 +131,55 @@ module.exports = {
   },
 
   feed: [
-    // A default feed configuration object
     {
       path: "/feed.xml", // The route to your feed.
-      // async create(feed) {}, // The create function (see below)
-      create = feed => {
+      async create(feed) {
         feed.options = {
-          title: 'Naochael Jordan Blog',
-          link: 'https://naochael-jordan.github.io/blog/feed.xml',
-          description: '普段はフロントエンドエンジニアしながら、Webサービスとかアプリ開発してます。Nuxt, ReactNative, Electron, Vuex, Redux辺りをよく触ってます。',
-        }
+          title: "Naochael Jordan Blog",
+          link: "https://naochael-jordan.github.io/blog/feed.xml",
+          description:
+            "普段はフロントエンドエンジニアしながら、Webサービスとかアプリ開発してます。Nuxt, ReactNative, Electron, Vuex, Redux辺りをよく触ってます。"
+        };
 
         feed.addItem({
-          title: 'たいとる',
-          id: 'https://naochael-jordan.github.io/blog/',
-          link: 'https://naochael-jordan.github.io/blog/',
-          description: 'ですくりぷしょん',
-          content: 'こんてんつ'
-        })
+          title: "たいとる",
+          id: "https://naochael-jordan.github.io/blog/",
+          link: "https://naochael-jordan.github.io/blog/",
+          description: "ですくりぷしょん",
+          content: "こんてんつ"
+        });
 
-        // const files = fs.readdirSync("./static");
-        // const posts = files.filter(file => file.match(/.md$/)).map(file => {
-        //   let post = fm(fs.readFileSync(`static/${file}`, "utf8"));
-        //   post.fileName = file.replace(/.md$/, "");
-        //   return post;
-        // });
-        // posts.forEach(post => {
+        // sourceFileArray.forEach(item => {
+        //   const summary =
+        //     fileMap[
+        //       item.replace(".md", ".json").replace("/markdown/", "/json/")
+        //     ];
+        //   const note = require(path.resolve(
+        //     __dirname,
+        //     item.replace("/markdown/", "/json/").replace(".md", ".json")
+        //   ));
+        //   const post = {
+        //     title: summary.title,
+        //     url: "https://isoppp.com" + sourceFileNameToUrl(item),
+        //     description: summary.preview,
+        //     content: note.bodyHtml
+        //   };
+
         //   feed.addItem({
-        //     title: post.attributes.title,
+        //     title: post.title,
         //     id: post.url,
         //     link: post.url,
         //     description: post.description,
         //     content: post.content
-        //   })
-        // })
-
-        feed.addCategory('Nuxt.js')
+        //   });
+        // });
 
         feed.addContributor({
-          name: 'Naochael Jordan',
-          email: 'naochael.jordan@gmail.com',
-          link: 'https://naochael-jordan.github.io/blog/'
-        })
-      },
+          name: "NaochaelJordan",
+          email: "naochael.jordan@gmail.com",
+          link: "https://naochael-jordan.github.io/blog"
+        });
+      }, // The create function (see below)
       cacheTime: 1000 * 60 * 15, // How long should the feed be cached
       type: "rss2" // Can be: rss2, atom1, json1
     }
