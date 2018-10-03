aeb393d0970e7aad48eb3d6a2341a08c62ad5e94
naochael-jordan <naochael.jordan@gmail.com>
2018-10-04 06:23:02 +0900

    feat: add hatena bookmark

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -3,7 +3,16 @@
     <h1>{{ title }}</h1>
     <p class="date">{{ format(date, 'YYYY-MM-DD') }}</p>
     <div v-html="htmlContent" class="contents"></div>
-    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+    <!-- Twitter -->
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+    <!-- はてブ -->
+    <a href="http://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="basic-label-counter" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加">
+      <img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" />
+    </a>
+    <script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
   </div>
 </template>
 
