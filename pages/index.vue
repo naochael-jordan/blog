c000048809577e87acb93e1e6a1c359a68897df7
naochael-jordan <naochael.jordan@gmail.com>
2018-11-06 07:00:37 +0900

    fix: add month

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <section>
-    <h1>Blog</h1>
     <ul>
-      <li v-for="(post, index) in posts" :key="index">
-        <p class="date">{{ format(post.attributes.date, 'YYYY-MM-DD') }}</p>
-        <nuxt-link :to="`/${post.fileName}/`">
-          <p class="title">{{ post.attributes.title }}</p>
-        </nuxt-link>
+      <li v-for="(post, index) in posts" :key="index" v-once>
+        <p v-if="showMonth(post.attributes.date, index)" class="month">{{ year }}</p>
+        <div class="contents">
+          <p class="date">{{ format(post.attributes.date, 'YYYY年MM月DD日') }}</p>
+          <nuxt-link :to="`/${post.fileName}/`">
+            <p class="title">{{ post.attributes.title }}</p>
+          </nuxt-link>
+        </div>
       </li>
     </ul>
   </section>
@@ -19,9 +21,20 @@ import { format } from "date-fns";
 @Component({})
 export default class Index extends Vue {
   format = format;
+  year = "";
 
   get posts() {
     return this.$store.state.posts;
+  }
+
+  showMonth(date, index) {
+    const year = format(date, "YYYY");
+    if (year === this.year) {
+      return false;
+    } else {
+      this.year = year;
+      return true;
+    }
   }
 }
 </script>
@@ -37,16 +50,26 @@ section {
   ul {
     margin: 0 0 0 10px;
     padding: 0;
+    list-style: none;
   }
 
   li {
+    margin-bottom: 20px;
+  }
+
+  .contents {
     display: flex;
     align-items: center;
-    margin-bottom: 20px;
   }
 
   p {
     margin: 0;
+  }
+
+  .month {
+    margin: 0 0 20px -10px;
+    font-size: 2.4rem;
+    font-weight: bold;
   }
 
   .date {
@@ -70,17 +93,30 @@ section {
     }
 
     li {
-      display: block;
       margin-bottom: 20px;
     }
 
+    .contents {
+      display: block;
+    }
+
+    .month {
+      margin: 0 0 10px -10px;
+      font-size: 2rem;
+      font-weight: bold;
+    }
+
     .date {
-      font-size: 1.2rem;
+      font-size: 1.4rem;
     }
 
     .title {
       font-size: 1.6rem;
       line-height: 1.4;
+    }
+
+    a {
+      display: inline-block;
     }
   }
 }
--- plugins/init.js
+++ plugins/init.js
@@ -4,11 +4,14 @@ import fm from "front-matter";
 export default ({ store: { commit } }) => {
   if (process.server) {
     const files = fs.readdirSync("./markdown");
-    const posts = files.filter(file => file.match(/.md$/)).map(file => {
-      let post = fm(fs.readFileSync(`markdown/${file}`, "utf8"));
-      post.fileName = file.replace(/.md$/, "");
-      return post;
-    });
+    const posts = files
+      .filter(file => file.match(/.md$/))
+      .map(file => {
+        let post = fm(fs.readFileSync(`markdown/${file}`, "utf8"));
+        post.fileName = file.replace(/.md$/, "");
+        return post;
+      })
+      .reverse();
 
     commit("setPosts", posts);
   }
