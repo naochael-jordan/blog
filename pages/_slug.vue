8cdb617484389003c08e9b2e3e00388c16ab5d6c
naochael-jordan <naochael.jordan@gmail.com>
2019-05-07 23:31:45 +0900

    fix route path

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -9,7 +9,7 @@ import { Vue, Component } from "nuxt-property-decorator";
 export default class Slug extends Vue {
   beforeCreate() {
     if (process.browser) {
-      location.href = `https://naokiotsu.github.io/blog${this.$route.path}`;
+      location.href = `https://naoki-otsu.com/blog${this.$route.path}`;
     }
   }
 }
